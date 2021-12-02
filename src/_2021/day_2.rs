68212d90c237ab877339f0150ad08f5580cba63c
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-12-02 06:48:51 +0100

    Add day 2/2021

---
+++ src/_2021/day_2.rs
@@ -0,0 +1,43 @@
+use crate::Day;
+
+pub fn new() -> Day2 { Day2 }
+
+pub struct Day2;
+impl Day for Day2 {
+	fn part_1(&mut self, input: &str) -> String {
+		let mut horizontal = 0;
+		let mut depth = 0;
+		for l in input.lines() {
+			let mut parts = l.split(" ");
+			let action = parts.next().unwrap();
+			let value = parts.next().unwrap().parse::<usize>().unwrap();
+			match action {
+				"forward" => horizontal += value,
+				"down" => depth += value,
+				"up" => depth -= value,
+				_ => panic!("Unknown instruction: {}", action),
+			}
+		}
+
+		(horizontal * depth).to_string()
+	}
+
+	fn part_2(&mut self, input: &str) -> String {
+		let mut horizontal = 0;
+		let mut depth = 0;
+		let mut aim = 0;
+		for l in input.lines() {
+			let mut parts = l.split(" ");
+			let action = parts.next().unwrap();
+			let value = parts.next().unwrap().parse::<usize>().unwrap();
+			match action {
+				"forward" => { horizontal += value; depth += aim * value; },
+				"down" => aim += value,
+				"up" => aim -= value,
+				_ => panic!("Unknown instruction: {}", action),
+			}
+		}
+
+		(horizontal * depth).to_string()
+	}
+}
--- src/_2021/mod.rs
+++ src/_2021/mod.rs
@@ -1,10 +1,12 @@
 use crate::Day;
 
 mod day_1;
+mod day_2;
 
 pub fn new(day: i32) -> Option<Box<dyn Day>> {
 	match day {
 		1 => Some(box day_1::new()),
+		2 => Some(box day_2::new()),
 		_ => None
 	}
 }
